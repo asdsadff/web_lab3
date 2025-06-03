3bd970cc6c423e38486155a061fc0c30e5a55ea6
Partsei Ihor <99366405+asdsadff@users.noreply.github.com>
2025-06-03 19:48:01 +0300

    Add files via upload

---
+++ web_lab3/src/test/java/com/partsey/ssl/SslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partsey.ssl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
